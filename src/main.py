a7d3e357b5d8d245d1b1cfe83da8a25663f50abb
Sune <enxbox@hotmail.com>
2020-02-17 11:20:06 +0100

    Pin test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 # main.py -- put your code here!
 import pycom
 import time
+import machine
 
-pycom.heartbeat(False)
+
+adc = machine.ADC()           # create an ADC object
+apin = adc.channel(pin='P17') # Listening to Pin 17
+
+while (True):
+    time.sleep(0.5)
+    value = apin()
+    print("Val = %d" %(value))
