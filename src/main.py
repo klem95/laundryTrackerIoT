89ff140d7c155d5c935834be8a1611947f806b50
Sune <enxbox@hotmail.com>
2020-03-09 16:00:15 +0100

    Full rework + add movingAvarage

--- src/main.py
+++ src/main.py
@@ -1,31 +1,42 @@
+
 import pycom
 import time
 import machine
 import networkSetup
 import packageConverter
 
+
 messenger = networkSetup.linkup() # Sending Object
 
 off = 0x000000
 green = 0x00ff00
-
-i = 0
+blue = 0x0000ff
 
 adc = machine.ADC()           # create an ADC object
 apin = adc.channel(pin='P17') # Listening to Pin 17
 
+sampleFrq = 10
+sampleCount = 0
+vibrations = 0
+
 while True:
     try:
         if messenger is not None:
-            value = apin()
-            package = packageConverter.shortConverter(value)
-            messenger.send(package)
-            print('Sent %s bytes' % package)
-
+            vibrationValue = apin()
+            vibrations += vibrationValue
+            if sampleCount > sampleFrq:
+                movingAvarage = int(vibrations / sampleFrq)
+                package = packageConverter.shortConverter(movingAvarage)
+                pycom.rgbled(blue)
+                messenger.send(package)
+                pycom.rgbled(off)
+                sampleCount = 0
+                vibrations = 0
+            sampleCount += 1
             pycom.rgbled(green)
-            time.sleep(0.1)
+            time.sleep(0.05)
             pycom.rgbled(off)
-            time.sleep(0.4)
+            time.sleep(0.05)
         pass
     except Exception as e:
         print(e)
